412a1e906ba15541751a68cd252c3a16ffabaf98
Bishoy Hanna Ezeet <42085452+BishoyHanna0@users.noreply.github.com>
2022-02-27 09:18:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,60 +1,59 @@
 <?php
-
-
- /*
-  - Google Recaptcha Integration PHP
-  - Script Writed By : Bishoy Hanna 
- */
+/*
+    - Google Recaptcha Integration PHP
+    - Script Writed By : Bishoy Hanna 
+*/
 
 # Sitekey , Secret key Get From Google Recaptcha admin Console (https://www.google.com/recaptcha/about/)
-
 $SiteKey = "6LdiOrsZAAAAAOE5hLFlO1c8OoGO7hvMxn3mhHj0";
 $SecretKey = "6LdiOrsZAAAAAPjCa7cy5KSGfQJJ2pU5jLBL50zu";
 
-
 # Check Recaptcha For User
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    if (isset($_POST["SendTest"]))
+    {
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-       if(isset($_POST["SendTest"])){
-           
-       
-                        if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
-                  // Google secret API
-                  $secretAPIkey = $SecretKey;
-                  
-                  // reCAPTCHA response verification
-                  $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secretAPIkey.'&response='.$_POST['g-recaptcha-response']);
-                  
-                  // Decode JSON data
-                  $response = json_decode($verifyResponse);
-                  
-                  
-                      if($response->success){
-                          
-                          echo 'Hello User';
-                          
-                      }
-                        }else{
-                            echo 'Please verify that you are not a robot
-';
-                            
-                        }
-        
+        if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response']))
+        {
+            // Google secret API
+            $secretAPIkey = $SecretKey;
+
+            // reCAPTCHA response verification
+            $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secretAPIkey . '&response=' . $_POST['g-recaptcha-response']);
+
+            // Decode JSON data
+            $response = json_decode($verifyResponse);
+
+            if ($response->success)
+            {
+
+                echo 'Hello User';
+
+            }
         }
+        else
+        {
+            echo 'Please verify that you are not a robot
+   ';
+
+        }
+
     }
+}
 
 ?>
 
-  <html>
-<head>
+<html>
+   <head>
       <title>Google Recaptcha Integration PHP</title>
-      </head>
-      <body>
-          <form action="" method="POST">
-                                            <div class="g-recaptcha" data-sitekey="<?php echo $SiteKey; ?>"></div>
-
-          <input type="submit" name="SendTest" value="Send ME! ">
-          </form>
-             <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-      </body>
+   </head>
+   <body>
+      <form action="" method="POST">
+         <div class="g-recaptcha" data-sitekey="<?php echo $SiteKey; ?>"></div>
+         <input type="submit" name="SendTest" value="Send ME! ">
+      </form>
+      <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+   </body>
 </html>
+
